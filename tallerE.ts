63ac13d0aa5446aaf43d86ec2d0759d239b3bc32
Bryanhurtado0006 <bryandreshurtado18@gmail.com>
2025-03-14 18:41:22 -0500

    Actualizando el tallerE.ts con mejoras

--- tallerE.ts
+++ tallerE.ts
@@ -39,15 +39,15 @@ function IMC(
 function analizarTemperaturas(ciudades: { ciudad: string; temperatura: number }[]) {
 
     let ciudadMasCaliente = ciudades[0];
-    let ciudadMasFria = ciudades[0];
+    let ciudadmasFria = ciudades[0];
     let sumaTemperaturas = 0;
 
     ciudades.forEach((ciudad) => {
         if (ciudad.temperatura > ciudadMasCaliente.temperatura) {
             ciudadMasCaliente = ciudad;
         }
-        if (ciudad.temperatura < ciudadMasFria.temperatura) {
-            ciudadMasFria = ciudad;
+        if (ciudad.temperatura < ciudadmasFria.temperatura) {
+            ciudadmasFria = ciudad;
         }
         sumaTemperaturas += ciudad.temperatura;
     });
@@ -56,19 +56,19 @@ function analizarTemperaturas(ciudades: { ciudad: string; temperatura: number }[
 
     return {
         ciudadMasCaliente: ciudadMasCaliente.ciudad,
-        ciudadMasFria: ciudadMasFria.ciudad,
+        ciudadMasFria: ciudadmasFria.ciudad,
         promedioTemperatura: promedio,
     };
 }
 
 
-const ciudades = [
+const citys = [
     { ciudad: "cali", temperatura: 32 },
     { ciudad: "barrancabermeja", temperatura: 22 },
     { ciudad: "washington DC", temperatura: 42 },
 ];
 
-const resultadot = analizarTemperaturas(ciudades);
+const resultadot = analizarTemperaturas(citys);
 console.log(resultadot);
 
 
